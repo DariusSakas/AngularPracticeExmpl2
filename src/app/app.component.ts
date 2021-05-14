073a6eab98681afe9beb7c3ff09d7d922c23bd0c
Darius <sakas.darius@gmail.com>
2021-05-14 21:04:37 +0300

    ngClass, ngFor, ngIf, ngSwitch,  ngStyle, ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularPracticeExmpl2';
+  title = 'initial';
+  constructor() {}
+  onInput(event: any) {
+    this.title = event.target.value;
+  }
+  backgroundToogle = false;
+  toogle = false;
+
+  arr = [1, 1, 2, 3, 5, 8, 13];
+  objs = [
+    {
+      title: 'Post1',
+      author: 'Kebab1',
+      comments: [
+        { name: 'Me', text: 'lorem' },
+        { name: 'Me', text: 'lorem' },
+        { name: 'Me', text: 'lorem' },
+      ],
+    },
+    {
+      title: 'Post2',
+      author: 'Kebab2',
+      comments: [
+        { name: 'Me2', text: 'lorem2' },
+        { name: 'Me2', text: 'lorem2' },
+        { name: 'Me2', text: 'lorem2' },
+      ],
+    },
+  ];
 }
